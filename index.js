589a16f6dc651d44f823781a20e093c73c9d47ea
hafizikram <contactme.ikram@gmail.com>
2022-03-22 21:16:23 +0530

    swagger

--- index.js
+++ index.js
@@ -5,9 +5,30 @@ const apiroutes = require('./routes/api');
 const invroutes = require('./routes/inventory');
 const bblogin = require("./validation/bblogin");
 const donor = require('./routes/donor')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+const swaggerAutogen = require('swagger-autogen')();
 
 const cors = require('cors');
 require('dotenv/config')
+
+
+
+// swagger config
+const doc = {
+	info: {
+		title: 'BloodBank API',
+		description: 'BloodBank spec for Swagger',
+	},
+	schemes: ['http', 'https'],
+};
+
+const outputFile = './docs/swagger.json';
+const endpointsFiles = ['./index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+app.use(`/v1/docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.use(express.json());
